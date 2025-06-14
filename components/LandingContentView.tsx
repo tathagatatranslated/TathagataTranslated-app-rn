c48280fb2cb7165bbff94acde23b338779307181
TathagataTranslated <TathagataTranslated@users.noreply.github.com>
2025-06-14 15:36:10 -0500

    last updated; updated homepage quote

--- components/LandingContentView.tsx
+++ components/LandingContentView.tsx
@@ -13,15 +13,21 @@ let LandingContentView: React.FC<LandingContentViewProps> = function(props)
             source={require('../resources/specific/index_background.jpg')}
             style={{ width: 313, height: 199, marginHorizontal: 'auto' }}
         />
-        <Text style={{ width: '65%', minWidth: 320, maxWidth: 660, marginHorizontal: 'auto', fontFamily: 'monospace', paddingBottom: 8, paddingTop: 6, fontSize: 15, borderLeftWidth: 3, backgroundColor: 'rgb(238, 238, 238)', borderLeftColor: '#ccc', paddingLeft: 10, paddingRight: 6, marginBottom: 6, marginTop: 3 }}>There is something we can not believe in the world.{'\n'}
-            However, as time goes by, people come to know such a thing.{'\n'}
-            And then it is clear that truth and falsehood can be proven by a fact.{'\n'}
-            {'\n'}
-            Who are you?{'\n'}
-            > I am a Tathagata.{'\n'}
+        <Text style={{ width: '65%', minWidth: 320, maxWidth: 660, marginHorizontal: 'auto', fontFamily: 'monospace', paddingBottom: 8, paddingTop: 6, fontSize: 15, borderLeftWidth: 3, backgroundColor: 'rgb(238, 238, 238)', borderLeftColor: '#ccc', paddingLeft: 10, paddingRight: 6, marginBottom: 6, marginTop: 3 }}>There are things in this world that cannot be believed.{'\n'}
+However, with time, people{'\n'}
+come to understand even those things.{'\n'}
+What is clear even then is only this:{'\n'}
+that truth and falsehood are revealed{'\n'}
+within a single reality.{'\n'}
+{'\n'}
+Q: Who are you?{'\n'}
+A: I am a Tathagata.{'\n'}
         </Text>
         <Text style={{ fontStyle: 'italic', width: '65%', minWidth: 320, maxWidth: 660, marginHorizontal: 'auto', fontSize: 12 }}>
-            New Translations {'>'} Books - Poetry: The Traveler, Chapter 9
+            from AI Translations {'>'} Books - Poetry: Traveler, Chapter 9
+        </Text>
+        <Text style={{ fontStyle: 'italic', width: '65%', minWidth: 320, maxWidth: 660, marginHorizontal: 'auto', fontSize: 12 }}>
+            Last Updated: May 22 2025
         </Text>
         {
             props.is_for_loading_splash ? (<Text style={{ fontStyle: 'normal', fontWeight: 'bold', width: '65%', minWidth: 320, maxWidth: 660, marginHorizontal: 'auto', fontSize: 17, marginTop: 20 }}>Downloading Content... Please Wait...</Text>) : (<></>)
